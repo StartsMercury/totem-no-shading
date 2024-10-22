9de45840a4cb7af490bcafabceddd390bbff7453
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-10-22 11:47:28 +0800

    Update to Minecraft 1.21.2 Release Candidate 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.1.1"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "1.21.2-rc1"
+    const val VERSION_MINECRAFT = "1.21.2-rc2"
 }
 
 plugins {
@@ -56,7 +56,7 @@ tasks.withType<ProcessResources> {
     val data = mapOf(
         "version" to Constants.VERSION,
         "version_java" to Constants.VERSION_JAVA,
-        "version_minecraft" to "1.21.2-rc.1",
+        "version_minecraft" to "1.21.2-rc.2",
     )
 
     inputs.properties(data)
