897b38bf91e3d8f81df5083f65b949311148744a
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-10-31 11:52:44 +0800

    Update to Minecraft 24w44a

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.1.1"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "1.21.3"
+    const val VERSION_MINECRAFT = "24w44a"
 }
 
 plugins {
@@ -56,7 +56,7 @@ tasks.withType<ProcessResources> {
     val data = mapOf(
         "version" to Constants.VERSION,
         "version_java" to Constants.VERSION_JAVA,
-        "version_minecraft" to Constants.VERSION_MINECRAFT,
+        "version_minecraft" to "1.21.4-alpha.24.44.a",
     )
 
     inputs.properties(data)
