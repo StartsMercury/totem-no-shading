9553c9037c03395801a67df71386ed49b5a3141a
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-08-30 05:00:29 +0800

    Update Iris to 1.9.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.17.2")
 
-    modCompileOnly("maven.modrinth:iris:1.9.0+1.21.6-fabric")
+    modCompileOnly("maven.modrinth:iris:1.9.1+1.21.7-fabric")
 }
 
 tasks.withType<ProcessResources> {
