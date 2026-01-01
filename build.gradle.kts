8379101ec71f105d0aa62d2ed4c42b82eb1ef589
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2026-01-01 19:58:06 +0800

    Update Iris to 1.10.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.18.4")
 
-    modCompileOnly("maven.modrinth:iris:1.9.7+1.21.10-fabric")
+    modCompileOnly("maven.modrinth:iris:1.10.4+1.21.11-fabric")
 }
 
 tasks.withType<ProcessResources> {
