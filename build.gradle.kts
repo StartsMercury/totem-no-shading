1158ce210f5b0ba0b10c3ef3102ef26745884b5a
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2026-02-19 13:54:12 +0800

    Update to Minecraft 26.1 Snapshot 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.1.2"
 
     const val VERSION_JAVA = 25
-    const val VERSION_MINECRAFT = "26.1-snapshot-8"
+    const val VERSION_MINECRAFT = "26.1-snapshot-9"
 }
 
 plugins {
@@ -55,7 +55,7 @@ tasks.withType<ProcessResources> {
     val data = mapOf(
         "version" to Constants.VERSION,
         "version_java" to Constants.VERSION_JAVA,
-        "version_game" to "26.1-alpha.8",
+        "version_game" to "26.1-alpha.9",
         "version_minecraft" to Constants.VERSION_MINECRAFT,
     )
 
